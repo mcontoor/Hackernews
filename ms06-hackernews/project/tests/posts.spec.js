98613b19fc17a7b2e73e9284a4948023b08fd75a
Shiva Bandari <bshivagoud@gmail.com>
2019-04-09 16:55:25 +0530

    setup basic test cases for /addpost route

---
+++ ms06-hackernews/project/tests/posts.spec.js
@@ -0,0 +1,32 @@
+const request = require('request-promise');
+
+describe('Posts Modules', function(){
+
+  it('Creates a post when a req is sent to /addpost', function(){
+    // testing code here
+    // todo: Incomplete
+    return request('http://localhost:8080/addpost?authtoken=a76abca78a8acabda87fbfb8')
+    .then(function(res){
+      expect(res).toBeDefined();
+      expect(res.message).toEqual('Success')
+      //assert(res.responseType === 500 );
+      //assert(res.message === "Needs authentication");
+    })
+  })
+
+  it('Sends an error in the response when a req is sent to /addpost without logging in', function(){
+    // Send a request without any authenticatio token 
+    // The response has to be a failure with proper error message that says
+    // "Needs authentication"
+
+    return request('http://localhost:8080/addpost')
+    .then(function(res){
+      expect(res).toBeNotDefined();
+    })
+    .catch(err=>{
+      expect(err.statusCode).toEqual(500);
+    })
+
+  })
+
+});
